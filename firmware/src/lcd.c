20028dc3aff6bd5ebd76a7cef1fcc3fbd433f514
Piotr Esden-Tempski <piotr@esden.net>
2017-07-05 20:29:52 -0700

    Increase the LCD DMA data clock speed to 12mHz.

--- firmware/src/lcd.c
+++ firmware/src/lcd.c
@@ -534,8 +534,8 @@ static void start_video_dma(const pixslice_impl *impl)
         TIM8_PSC    = 0;
         // TIM8_ARR    = 34/2;
         // TIM8_CCR3   = 17/2;
-        TIM8_ARR    = 17 + 2;
-        TIM8_CCR3   = 12 + 4;
+        TIM8_ARR    = 11 + 2;
+        TIM8_CCR3   = 5 + 4;
         TIM8_BDTR   = TIM_BDTR_MOE | TIM_BDTR_OSSR;
         // TIM8_DCR    = 0;
         // TIM8_DMAR   = 0;
