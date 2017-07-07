57f3fa73ecea40ed00d196b5137ba2c81f2a011b
Piotr Esden-Tempski <piotr@esden.net>
2017-07-07 10:52:43 -0700

    Some initial fooling around with tile rendering.

--- firmware/src/tile_app.c
+++ firmware/src/tile_app.c
@@ -28,16 +28,96 @@
 
 gfx_rgb565 color = 0;
 
-static int16_t tile_y_inc = +1;
-static int16_t tile_y = 0;
+//static int16_t tile_y_inc = +1;
+//static int16_t tile_y = 0;
+
+static int16_t map_x_off = 0;
+static int16_t map_x_dir = +1;
+
+static struct tile_video_state {
+	uint16_t const *tilemap;
+	uint16_t tilemap_w;
+	uint16_t tilemap_h;
+	uint16_t const *tilesheet;
+	uint16_t tilesheet_w;
+	uint16_t tilesheet_h;
+	uint16_t x_off;
+	uint16_t y_off;
+} tile_video_state;
 
 void tile_init(void)
 {
+	tile_video_state.tilemap = &tml1_tilemap[0][0][0];
+	tile_video_state.tilemap_w = TML1_TILEMAP_WIDTH;
+	tile_video_state.tilemap_h = TML1_TILEMAP_HEIGHT;
 
+	tile_video_state.tilesheet = &ts_pixmap[0][0];
+	tile_video_state.tilesheet_w = TS_PIXMAP_WIDTH;
+	tile_video_state.tilesheet_h = TS_PIXMAP_HEIGHT;
+
+	tile_video_state.x_off = 0;
+	tile_video_state.y_off = 0;
+
+}
+
+void tile_draw_tile(gfx_pixslice *slice, uint16_t tile_id, int px, int py)
+{
+	int tile_x = (tile_id * 8) % TS_PIXMAP_WIDTH;
+	int tile_y = ((tile_id * 8) / TS_PIXMAP_WIDTH) * 8;
+
+	if (tile_x >= TS_PIXMAP_WIDTH)
+	{
+		return;
+	}
+
+	if (tile_y >= TS_PIXMAP_HEIGHT)
+	{
+		return;
+	}
+
+	for (int y = 0; y < 8; y++) {
+		gfx_rgb565 *px0 =
+			gfx_pixel_address_unchecked(slice, px, py + (y * 2));
+		gfx_rgb565 *px1 =
+			gfx_pixel_address_unchecked(slice, px, py + (y * 2) + 1);
+		for (int x = 0; x < 8; x++) {
+			if (ts_pixmap[tile_y + y][tile_x + x] != 0xF81F) {
+				*px0++ = ts_pixmap[tile_y + y][tile_x + x];
+				*px0++ = ts_pixmap[tile_y + y][tile_x + x];
+				*px1++ = ts_pixmap[tile_y + y][tile_x + x];
+				*px1++ = ts_pixmap[tile_y + y][tile_x + x];
+			} else {
+				px0+=2;
+				px1+=2;
+			}
+		}
+	}
+
+	#if 0
+	int tile_x = (tile_id * 8) % tile_video_state.tilesheet_w;
+	int tile_y = ((tile_id * 8) / tile_video_state.tilesheet_w) * 8;
+
+	for (int y = 0; y < 8; y++) {
+		gfx_rgb565 *px0 =
+			gfx_pixel_address_unchecked(slice, px, py + (y * 2));
+		gfx_rgb565 *px1 =
+			gfx_pixel_address_unchecked(slice, px, py + (y * 2) + 1);
+		for (int x = 0; x < 8; x++) {
+			int tile_pix =
+				((tile_y + y) * tile_video_state.tilesheet_w) +
+				(tile_x + x);
+			*px0++ = tile_video_state.tilesheet[tile_pix];
+			*px0++ = tile_video_state.tilesheet[tile_pix];
+			*px1++ = tile_video_state.tilesheet[tile_pix];
+			*px1++ = tile_video_state.tilesheet[tile_pix];
+		}
+	}
+	#endif
 }
 
 void tile_animate(void)
 {
+#if 0
     tile_y += tile_y_inc;
     if (tile_y + (TS_PIXMAP_HEIGHT*2) > LCD_HEIGHT) {
         tile_y_inc = -1;
@@ -46,6 +126,33 @@ void tile_animate(void)
         tile_y_inc = +1;
         tile_y += 1;
     }
+#endif
+	static uint16_t delay = 0;
+	static uint16_t stay = 0;
+
+	if (delay == 1) {
+		delay = 0;
+
+		map_x_off += map_x_dir;
+		if (map_x_off == 108*8) {
+			map_x_dir = 0;
+			stay++;
+			if (stay == 10) {
+				stay = 0;
+				map_x_dir = -1;
+			}
+		}
+		if (map_x_off == 0) {
+			map_x_dir = 0;
+			stay++;
+			if (stay == 10) {
+				stay = 0;
+				map_x_dir = +1;
+			}
+		}
+	} else {
+		delay++;
+	}
 }
 
 static void tile_render_slice(gfx_pixslice *slice)
@@ -65,6 +172,7 @@ static void tile_render_slice(gfx_pixslice *slice)
 	}
 #endif
 
+#if 0
 	for (size_t y = slice->y; y < (slice->y + slice->h); y++ ) {
             	int32_t yy = (int32_t)y - tile_y;
                 if (yy < 0 || yy >= (TS_PIXMAP_HEIGHT*2))
@@ -80,6 +188,20 @@ static void tile_render_slice(gfx_pixslice *slice)
 				}
 			}
 		}
+	}
+#endif
+
+//	if(slice->y == 0) {
+//		tile_draw_tile(slice, 1, 10, 10);
+//	}
+
+	for (size_t y = slice->y / 16; y < (slice->y + slice->h)/16; y++) {
+		for (size_t x = 0; x < slice->w / 16; x++) {
+			if (tml1_tilemap[0][y + 3][x + (map_x_off / 8)] != 0) {
+				tile_draw_tile(slice, tml1_tilemap[0][y + 3][x + (map_x_off / 8)]-1, x * 16 - (map_x_off % 8) * 2, y*16);
+			}
+		}
+
 	}
 
 }
