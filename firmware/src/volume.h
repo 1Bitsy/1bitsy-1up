7b58844b6cfb3a89673be8f19ad0a00895f37523
kbob <kbob@jogger-egg.com>
2018-03-23 09:32:17 -0400

    Update volume.h.

--- firmware/src/volume.h
+++ firmware/src/volume.h
@@ -1,21 +1,27 @@
 #ifndef VOLUME_included
 #define VOLUME_included
 
-#ifndef AUDIO_REPAIR
-
 #include <stdint.h>
 
-#define VOLUME_MIN 0
-#define VOLUME_MAX 38
-#define VOLUME_RAW_MAX 256
+#ifdef AUDIO_REPAIR
+    #define VOLUME_MIN 0
+    #define VOLUME_MAX 64
+    #define VOLUME_RAW_MAX 4095
+#else
+    #define VOLUME_MIN 0
+    #define VOLUME_MAX 38
+    #define VOLUME_RAW_MAX 256
+#endif
 
 extern void     volume_init    (void);
 extern uint8_t  volume_get     (void);
-extern void     volume_set     (uint8_t);
+#ifndef AUDIO_REPAIR
+    extern void volume_set     (uint8_t);
+#endif
 
 extern uint16_t volume_get_raw (void);
-extern void     volume_set_raw (uint16_t);
-
-#endif /* !AUDIO_REPAIR */
+#ifndef AUDIO_REPAIR
+    extern void volume_set_raw (uint16_t);
+#endif
 
 #endif /* !VOLUME_included */
